96593ae0c3688c11274bb37b3c1ff0bb91c5f4d9
96jonesa <ajones96@uw.edu>
2020-10-15 15:27:30 -0700

    smooth movement off when not dancing

--- js/dance_sim.js
+++ js/dance_sim.js
@@ -236,7 +236,7 @@ function simTick() {
         }
     }
 
-    if (simSmoothMovement) {
+    if (simSmoothMovement && plIsDancing[simCurrentGroup]) {
         subTick = 1.0;
 
         if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
